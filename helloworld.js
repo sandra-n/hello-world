1911cb7ac25482fede2b81dfd8f939e3ac925eb6
sandra-n <sandracomp.obi@gmail.com>
2019-05-03 16:46:11 -0300

    Delete helloworld.js

--- helloworld.js
+++
@@ -1,8 +0,0 @@
-var http = require('http')
-var frase = 'Testando variaveis'
-
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello World');
-    res.end(frase);
-}).listen(8080);
