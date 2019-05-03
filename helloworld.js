1c91146709dfa6e006a492cc6db63a8f296e46f6
Taqtile <taqtile@Duvel.local>
2019-05-03 11:03:53 -0300

    atualizacao do arquivo

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,8 @@
 var http = require('http')
+var frase = 'Testando variaveis'
 
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World');
+    res.write('Hello World');
+    res.end(frase);
 }).listen(8080);
