3260320297b301b689a1aefd7636a3a5ac6c102c
Sandra Nihama <sandracomp.obi@gmail.com>
2019-05-12 15:36:38 -0300

    correção no código (eliminação de comentários e debug

--- src/telalogin/login.ts
+++ src/telalogin/login.ts
@@ -1,17 +1,8 @@
 import { AuthenticationMaker } from './checkuser';
-import { Pool } from 'pg';
 
 export function performLogin(req, res){
     const emailGiven = req.body.email;
     const pwGiven = req.body.password;
-
-    let pool = new Pool({
-        user: 'sandra',
-        host: 'localhost',
-        database: 'listausuarios',
-        password: 'login',
-        port: 5432,
-    });
 
     let objAM = new AuthenticationMaker();
     objAM.verifyUser(emailGiven, pwGiven, res);
