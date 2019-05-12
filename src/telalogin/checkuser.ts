1ec88bc8da74fc218a48cb892e90e408c305e073
Sandra Nihama <sandracomp.obi@gmail.com>
2019-05-12 16:58:46 -0300

    alteracao no checkuser

--- src/telalogin/checkuser.ts
+++ src/telalogin/checkuser.ts
@@ -1,24 +1,14 @@
-import { Pool } from 'pg';
 import { calculateHash } from './hashing';
 import { queryErrors } from './errors';
-import { verifyToken } from './token';
-//import { UsersList } from '/Users/taqtile/hello-world/src/onlineusers/findusers';
+import { pool } from '../index';
 
 export class AuthenticationMaker {
-    pool: Pool;
+  verifyUser(email, password, res) {
+    var hashUser = calculateHash(password);
+    console.log('hashUser: '+hashUser);
 
-    constructor (poolP: Pool) {
-        this.pool = poolP;
-    }
-
-    verifyUser(email, password, res) {
-        var hashUser = calculateHash(password);
-        console.log('hashUser: '+hashUser);
-
-        this.pool.query('SELECT * FROM usuarios WHERE email = $1 AND hash = $2', [email, hashUser], (error, results) => {
-            queryErrors(error, results, res, email, hashUser);
-        })
-
-    //const obj = new UsersList(this.pool);
-    }
+    pool.query('SELECT * FROM usuarios WHERE email = $1 AND hash = $2', [email, hashUser], (error, results) => {
+      queryErrors(error, results, res, email, hashUser);
+    })
+  }
 }
